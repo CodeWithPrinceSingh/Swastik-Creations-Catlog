79a5e4ad075056ae5d87f4a98c74a947d950c2f8
CodeWithPrinceSingh <111429747+CodeWithPrinceSingh@users.noreply.github.com>
2026-07-03 13:57:26 +0530

    Add files via upload

--- server/src/controllers/robotsController.js
+++ server/src/controllers/robotsController.js
@@ -1,13 +1,7 @@
 export const getRobotsTxt = (req, res) => {
-  const SITE_URL = (process.env.CLIENT_ORIGIN || 'https://swastik-creations-catlog.vercel.app').replace(/\/$/, '');
+  const SITE_URL = 'https://swastik-creations-catlog.vercel.app';
 
-  const content = `User-agent: Googlebot
-Disallow: /nogooglebot/
-
-User-agent: *
-Allow: /  
-  Sitemap: ${SITE_URL}/sitemap.xml
-`;
+  const content = `User-agent: *\nAllow: /\n\nSitemap: ${SITE_URL}/sitemap.xml\n`;
 
   res.set('Content-Type', 'text/plain');
   res.send(content);
